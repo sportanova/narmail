56e9cc57bdf09e70a6fa3a1b58887a5cbe0c6580
sportanova <sportano@gmail.com>
2014-09-24 23:36:18 -0700

    put futures @ top to give tiny bit more time to execute (mostly pointless)

--- src/main/scala/com/textMailer/IO/actors/SaveEmailDataActor.scala
+++ src/main/scala/com/textMailer/IO/actors/SaveEmailDataActor.scala
@@ -26,7 +26,7 @@ class SaveEmailDataActor extends Actor {
   import com.textMailer.IO.actors.SaveEmailDataActor._
   def receive = {
     case SaveData(userId, to, cc, bcc, emailAddress, sender, subject, ts, threadId, gmId, emailAccountId, body) => {
-      val recipients: TreeSet[String] = TreeSet[String]() ++ to.map(_.toString) ++ bcc.map(_.toString) ++ cc.map(_.toString) - emailAddress + sender
+      val recipients: TreeSet[String] = TreeSet[String]() ++ to ++ bcc ++ cc - emailAddress + sender
       println(s"@@@@@@@@@@@ recipientsSet $recipients")
       val recipientsString = recipients.toString
       println(s"@@@@@@@@@@@ recipientsString $recipientsString")
@@ -35,6 +35,10 @@ class SaveEmailDataActor extends Actor {
       println(s"############## timestamp $ts \n\n\n")
       println(s"!!!!!!!!!!!! subject: $subject")
       println(s"!!!!!!!!!!!! threadId: $threadId")
+      
+      // get futures started
+      val topicCount = TopicIO().asyncCount(List(Eq("user_id", userId), Eq("recipients_hash", recipientsHash)), 100).map(c => c)
+      val emailCount = EmailIO().asyncCount(List(Eq("user_id", userId), Eq("recipients_hash", recipientsHash)), 100).map(c => c)
       
       val textBody = body.get("text") match {
         case Some(t) => t.toString
@@ -49,10 +53,8 @@ class SaveEmailDataActor extends Actor {
       val topic = Topic(userId, recipientsHash, threadId, subject, ts)
       TopicIO().asyncWrite(topic)
       OrdTopicIO().asyncWrite(topic)
-      val email = Email(gmId, userId, threadId, recipientsHash, Some(recipients), ts, subject, sender, "cc", "bcc", textBody, htmlBody)
+      val email = Email(gmId, userId, threadId, recipientsHash, Some(recipients), ts, subject, sender, cc.toString, bcc.toString, textBody, htmlBody)
       EmailIO().asyncWrite(email)
-//      val topicCount = TopicIO().asyncCount(List(Eq("user_id",email.userId), Eq("recipients_hash", recipientsHash)), 100).map(c => )
-      val emailCount = EmailIO().asyncCount(List(Eq("user_id",email.userId), Eq("recipients_hash", recipientsHash)), 100).map(c => c)
       val conversation = Conversation(userId, recipientsHash, recipients, ts, emailAccountId) // do this last, give time to get topic count
       ConversationIO().asyncWrite(conversation)
       OrdConversationIO().asyncWrite(conversation)
